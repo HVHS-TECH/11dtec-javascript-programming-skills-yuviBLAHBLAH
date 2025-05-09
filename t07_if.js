32b054818519f15c797128565ddf39e8b6bd0914
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-09 03:08:57 +0000

    May 9, 2025, 3:08 AM

---
+++ t07_if.js
@@ -0,0 +1,25 @@
+/****************************
+Name of task: Intro to JavaScript
+Person who wrote it: Yuvi
+Date written: 29/04/2025
+Purpose: Introduction to JavaScript and how it works
+****************************/
+console.log("hello zlunc 3");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+if (age < 5){
+    console.log(YOU TOO YOUNG WHAT THE HELL😨😨🤬🤬🤬😡😡)
+}
+if (age > 15){
+    console.log(what in the unc is this🤣🤣🤣💀💀)
+}
+if (age > 13){
+    console.log(thats just right 💘💝💝😍😍😍😍😘😘)
+}
+/****************************
+Functions
+****************************/
