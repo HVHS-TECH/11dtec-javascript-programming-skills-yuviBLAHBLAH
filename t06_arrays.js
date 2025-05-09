84e45d9b1b2e95b1f7d1669c5b0131677d59d9a3
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-09 02:58:09 +0000

    May 9, 2025, 2:58 AM

---
+++ t06_arrays.js
@@ -0,0 +1,20 @@
+/****************************
+Name of task: Intro to JavaScript
+Person who wrote it: Yuvi
+Date written: 29/04/2025
+Purpose: Introduction to JavaScript and how it works
+****************************/
+console.log("hello zlunc");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+let classArray = ["Yuvi", "Yuvi jr", "Yuvi jr jr", "Yuvi jr jr jr", "Yuvi jr jr jr jr"]
+let choice =
+
+
+/****************************
+Functions
+****************************/
