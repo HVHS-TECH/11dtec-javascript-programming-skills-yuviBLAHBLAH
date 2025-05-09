8d3b70b8a40d8a528b8b70fee57b3a16bd72e583
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-09 03:03:31 +0000

    May 9, 2025, 3:03 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -4,15 +4,16 @@ Person who wrote it: Yuvi
 Date written: 29/04/2025
 Purpose: Introduction to JavaScript and how it works
 ****************************/
-console.log("hello zlunc");
+console.log("hello zlunc 2");
 
 //Variables
 
 /****************************
 Main code
 ****************************/
-let classArray = ["Yuvi", "Yuvi jr", "Yuvi jr jr", "Yuvi jr jr jr", "Yuvi jr jr jr jr"]
-let choice =
+let classArray = ["Yuvi❤️❤️❤️❤️", "Yuvi jr🤑🤑💵💵", "Yuvi jr jr🤣🤣😂😂", "Yuvi jr jr jr💀💀👲🏻👲🏻", "Yuvi jr jr jr jr🤩🤩🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔"];
+let choice = prompt("pick a number between 0 and 4 zlunc👌👌👌👌");
+alert("person "+choice+" is "+classArray[choice]);
 
 
 /****************************
