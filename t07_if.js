7ccd261532a0d89e0571606c7639e8cc717ef625
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-14 00:11:21 +0000

    May 14, 2025, 12:11 AM

--- t07_if.js
+++ t07_if.js
@@ -13,16 +13,28 @@ Main code
 ****************************/
 alert("hello bro");
 alert("i know you were born in 2009");
+var age = prompt("How old is you are bluncDunk");
+var pocketMoney = prompt("How much rich are you😁😁🤑🤑🫰🫰💰💸💶💵💷💹💳💴🪙");
 
 if (age < 14){
-    console.log(so yung what 😨😨🤬🤬🤬😡😡)
+    console.log("so young what 😨😨🤬🤬🤬😡😡")
 }
 if (age > 16){
-    console.log(what in the unc is this🤣🤣🤣💀💀)
+    console.log("what in the unc is this🤣🤣🤣💀💀💀💀💀💀💀💀")
 }
 if (age == 15){
-    console.log(thats just right 💘)
+    console.log("thats just right❤️❤️❤️😍😍💕💕😘😘😁😁😁😁👌👌👌👌👌👌👌👌👌👌")
 }
+if (pocketMoney > 21){
+    console.log("ok rich guy💹💹💷💵💵💰💰🪙💸💸🫰🫰🫰🫰💳💶💵💵💵💵💵")
+}
+if (pocketMoney < 19){
+    console.log("DAMN UNC YOU POOR A PLUNK🤣🤣🤣🤣")
+}
+if (pocketMoney == 20){
+    console.log("you are choclatety 👅👅👅")
+}
+
 /****************************
 Functions
 ****************************/
---
+++ t08_for_loop.js
@@ -0,0 +1,18 @@
+/****************************
+Name of task: Intro to JavaScript
+Person who wrote it: Yuvi
+Date written: 29/04/2025
+Purpose: Introduction to JavaScript and how it works
+****************************/
+console.log("hello zlunc 4💖💖🥰🥰😍😻😻😻💘💝💝");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+
+
+/****************************
+Functions
+****************************/
