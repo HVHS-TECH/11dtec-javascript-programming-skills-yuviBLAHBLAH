18e51ecc035202056c486272f2466371f3c9e8a1
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-15 01:04:30 +0000

    May 15, 2025, 1:04 AM

---
+++ t09_functions.js
@@ -0,0 +1,23 @@
+/****************************
+Name of task: Intro to JavaScript
+Person who wrote it: Yuvi
+Date written: 29/04/2025
+Purpose: Introduction to JavaScript and how it works
+****************************/
+console.log("hello zlunc 4🥰🥰");
+
+//Variables
+var answer;
+/****************************
+Main code
+****************************/
+console.log("Loop stopped!");
+ for (count=0;count<5;count++){
+  var nums =  prompt ("Pick a number")
+  nums = Number(nums)
+ }
+ answer = nums+nums+nums+nums+nums;
+ console.log(answer)
+/****************************
+Functions
+****************************/
