3ae3ae749a27b394ac7805efc69e1f5205856dd5
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-15 01:25:19 +0000

    May 15, 2025, 1:25 AM

--- t09_functions.js
+++ t09_functions.js
@@ -15,7 +15,31 @@ Main code
 /****************************
 Functions
 ****************************/
-function getFaColour() {
-    userFavColour = prompt("What is your fav colour?")
-    alert("Your fav color is" + userFavColour)
-}
+function getFavColor(){
+    userFavColor = prompt("What is your favourite color?");
+    alert ("Your favourite color is: " + userFavColor);
+};
+getFavColor();
+
+function getUserName(){
+    userabcdefghijlkmnopqrstuvwxyz = prompt("What's your name?")
+    alert("Welcome " + userabcdefghijlkmnopqrstuvwxyz);
+};
+getUserName();
+
+function askUserAge(){
+    userAge = prompt("How old are you?");
+    alert("Welcome " + userAge);
+};
+askUserAge();
+
+function askUserMoney(){
+   var userMoney = prompt("How much money do you have?")
+    if (userMoney < 4){
+        alert("Your too broke to buy chocolate");
+    };
+    if (userMoney >= 4){
+        alert("You can buy chocolate");
+    };
+};
+askUserMoney();
