1139ce7e89f916ec6c27280be292ac3b1242342c
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-16 02:34:31 +0000

    May 16, 2025, 2:34 AM

---
+++ t10_activate_via_button.js
@@ -0,0 +1,45 @@
+/****************************
+Name of task: Intro to JavaScript
+Person who wrote it: Yuvi
+Date written: 29/04/2025
+Purpose: Introduction to JavaScript and how it works
+****************************/
+console.log("hello zlunc 6    👈(ﾟヮﾟ👈)   ");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+
+/****************************
+Functions
+****************************/
+function getFavColor() {
+    userFavColor = prompt("What is your favourite color?");
+    alert("Your favourite color is: " + userFavColor);
+};
+getFavColor();
+
+function getUserName() {
+    userabcdefghijlkmnopqrstuvwxyz = prompt("What's your name?")
+    alert("Welcome " + userabcdefghijlkmnopqrstuvwxyz);
+};
+getUserName();
+
+function askUserAge() {
+    userAge = prompt("How old are you?");
+    alert("Welcome " + userAge + " year old👋🙋‍♂️🙋‍♂️🙋‍♂️");
+};
+askUserAge();
+
+function askUserMoney() {
+    var userMoney = prompt("How much money do you have?")
+    if (userMoney < 4) {
+        alert("Your too broke to buy chocolate😒🤣🤣🤣🤣🤣😒😒😒😒");
+    };
+    if (userMoney >= 4) {
+        alert("You can buy chocolatea😘😘😘💕💕👍👍😁😁😁");
+    };
+};
+askUserMoney();
