e25af56d617b70534ec12e81da35b7a042a51ecf
yuviBLAHBLAH <23096yb@hvhs.school.nz>
2025-05-28 00:02:41 +0000

    May 28, 2025, 12:02 AM

---
+++ t12_while_loop.js
@@ -0,0 +1,22 @@
+/************************************************************************************************************
+boi             
+ ***********************************************************************************************************/
+console.log("Running t12_while_loop.js");
+
+//Variables
+var tru = prompt("Do you want to continue with this loop?\nPick Y or N");
+
+/************************************************************************************************************ 
+ Main code 
+ ***********************************************************************************************************/
+while (tru != "N"){
+    tru = alert("Loop will play again");
+    var tru = prompt("Do you want to continue with this loop?\nPick Y or N");
+};
+if (tru = "N"){
+alert("Loop cancelled!");
+};
+
+function myFunction(){
+    confirm("Press this button to start this loop!\nIf you want to continue press it again\nIf not don't press it")
+}
